2b39520535b76e20532ab93f8e3411c211d90cea
Kristine Lee <kristinee.lee11@gmail.com>
2023-03-22 17:20:40 +0800

    Added sign in function

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@ import json
 import sqlalchemy
 from flask_bcrypt import Bcrypt
 from flask import Flask, request, Response, flash
-from forms import RegistrationForm, LoginForm
 from typing import Dict
 
 app = Flask(__name__, static_url_path='')  # Setup the flask app by creating an instance of Flask
@@ -24,23 +23,29 @@ engine = sqlalchemy.create_engine(
 db = engine.connect()
 
 @app.route('/')
-def home():  # At the same home function as before
-    return app.send_static_file('index.html')  # Return index.html from the static folder
+def home():
+    return app.send_static_file('index.html')
 
 @app.route('/sign-up', methods=['GET', 'POST'])
-# ? a flask decorator listening for POST requests at the url /table-insert and handles the entry insertion into the given table/relation
-# * You might wonder why PUT or a similar request header was not used here. Fundamentally, they act as POST. So the code was kept simple here
-def signup():
+def signUp():
     try:
         data = request.data.decode()
         user = json.loads(data)
         statement, checkEmail, checkUser = newUser(user)
+        emailRes = db.execute(checkEmail).fetchone()
+        userRes = db.execute(checkUser).fetchone()
+        if emailRes and userRes:
+            return Response("Account with this email and username already exists.", 403)
+        elif userRes:
+            return Response("Account with this username already exists.", 403)
+        elif emailRes:
+            return Response("Account with this email already exists.", 403)
         db.execute(statement)
         db.commit()
         return Response(statement.text, 200)
     except Exception as e:
         db.rollback()
-        return Response(str(e), 403)
+        return Response("Server problem.", 403)
 
 def newUser(user: Dict):
     username = user['username']
@@ -51,6 +56,23 @@ def newUser(user: Dict):
     checkEmail = sqlalchemy.text(f"SELECT * FROM users WHERE email LIKE '{email}'")
     checkUser = sqlalchemy.text(f"SELECT * FROM users WHERE username LIKE '{username}'")
     return statement, checkEmail, checkUser
+
+@app.route('/login', methods=['GET'])
+def signIn():
+    try:
+        email = request.args.get('email', default="", type=str)
+        password = request.args.get('password', default="", type=str)
+        statement = sqlalchemy.text(f"SELECT * FROM users WHERE email LIKE '{email}'")
+        checkUser = db.execute(statement).fetchone()
+        if checkUser and bcrypt.check_password_hash(checkUser[2], password):
+            userDetails = {'username': checkUser[0], 'email': checkUser[1]} #username and email
+            return userDetails
+        else:
+            return Response("Incorrect username/password", 403)
+    except Exception as e:
+        db.rollback()
+        return Response("Server problem.", 403)
+    
 
 if __name__ == '__main__':  # If the script that was run is this script (we have not been imported)
     app.run()  # Start the server
